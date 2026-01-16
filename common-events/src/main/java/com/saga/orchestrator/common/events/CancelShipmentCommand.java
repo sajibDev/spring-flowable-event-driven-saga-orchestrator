dfc62d6cb2d2b21c078e288bdc659d59921db5ac
mschowdhury <sajib.chowdhury@bracits.com>
2026-01-16 10:20:11 +0600

    feat: Add CancelShipmentCommand class for handling shipment cancellation events

---
+++ common-events/src/main/java/com/saga/orchestrator/common/events/CancelShipmentCommand.java
@@ -0,0 +1,19 @@
+package com.saga.orchestrator.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CancelShipmentCommand implements Serializable {
+    private String correlationId;
+    private String orderId;
+    private String shipmentId;
+}
+
